8d259ff77f563abbf9881def3941c947469cd700
eivindfc <eivind.christensen@bettermeetingrooms.com>
2025-07-10 17:19:23 +0200

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -37,7 +37,7 @@ const sidebars: SidebarsConfig = {
   type: 'category',
   label: 'Getting Started',
   items: [
-    'Getting Started/account-setup',
+    'getting-tarted/account-setup',
   ],
 },
   ],
