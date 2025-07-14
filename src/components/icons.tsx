2c9f194821e3a62e7d11062e9d25fa7c59f3e803
eivindfc <eivind.christensen@bettermeetingrooms.com>
2025-07-14 16:15:49 +0200

    Create icons.tsx

---
+++ src/components/icons.tsx
@@ -0,0 +1,21 @@
+// src/components/icons.tsx
+
+import {
+  ScanLine,
+  Mic,
+  Send,
+  MonitorSmartphone,
+  LayoutDashboard,
+  Settings,
+  Users,
+} from 'lucide-react';
+
+export {
+  ScanLine,
+  Mic,
+  Send,
+  MonitorSmartphone,
+  LayoutDashboard,
+  Settings,
+  Users,
+};
